6792146ef270c88047e5142af879b623b9e6ac78
david ramirez <davidramirezrivera2511@gmail.com>
2022-09-02 08:22:38 -0500

    fucion de menu mobile y desktop a el navar

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesk = document.querySelector('.desktop-menu');
+const menuIconMobile = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuIconMobile.addEventListener('click',toggleMobileMenu)
 
 function toggleDesktopMenu(){
     menuDesk.classList.toggle('inactive'); //la opcion toggle hace que se quite o se ponga la clase inactive
+}
+function toggleMobileMenu(){
+    menuMobile.classList.toggle('inactive');
 }
